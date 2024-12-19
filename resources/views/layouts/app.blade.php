d1eb5a48d6ee304c4fbc7a9fe30837689de4e16d
Jamshed Javed <jamshady@gmail.com>
2024-12-19 11:28:35 +0000

    Init

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Digistorm test') }}</title>
+
+    <!-- Scripts -->
+    @vite('resources/js/app.js')
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div id="app">
+        <nav>
+            <div>
+                <a href="{{ url('/') }}">
+                    {{ config('app.name', 'Digistorm test') }}
+                </a>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
