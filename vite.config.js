87ba9cf8ec186f3634c2273600bbfff747e9f0a3
Jamshed Javed <jamshady@gmail.com>
2024-12-19 17:01:26 +0000

    Vite: HMR Support

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    const url = new URL(env.APP_URL || 'http://localhost')
+    const host = url.hostname
+
+    return {
+        server: {
+            hmr: {
+                host,
+            },
+        },
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+        ],
+    }
 });
